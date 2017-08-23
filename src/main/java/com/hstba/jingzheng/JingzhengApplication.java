7a06e28e200afc6fbfeebeb8624dca76acf4f063
LoadChange <soocto@gmail.com>
2017-08-23 10:14:21 +0800

    项目初始化

---
+++ src/main/java/com/hstba/jingzheng/JingzhengApplication.java
@@ -0,0 +1,12 @@
+package com.hstba.jingzheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JingzhengApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JingzhengApplication.class, args);
+    }
+}
